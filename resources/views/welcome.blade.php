4f807c1116d905824164a7f38ce79a29bce5bf17
tndwhs <ljjh067@gmail.com>
2019-08-14 10:20:07 +0800

    建立後台

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,8 @@
-@extends('layouts.app')
+@extends('layouts.app')S
 
 @section('content')
 <div class="row justify-content-center">
-    <div class="col-md-8">
+    <div class="col-md-12S">
         <h1>商品一覽表</h1>
     </div>
 </div>
